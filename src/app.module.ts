577258f833cbd3319148dfd56d0fd60f725ff86c
Aliver <aliver.len@outlook.com>
2025-07-26 20:32:52 +0800

    chore: upgrade deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ControllerModule } from './controller/controller.module';
 import { DomainModule } from './domain/domain.module';
 import { RepositoryModule } from './repository/repository.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
@@ -8,10 +7,9 @@ import { CommonModule } from './common/common.module';
 @Module({
   imports: [
     InfrastructureModule,
-    ControllerModule,
     DomainModule,
     RepositoryModule,
     CommonModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
